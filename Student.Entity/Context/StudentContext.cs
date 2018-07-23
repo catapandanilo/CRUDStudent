d334891519e0a4e2002792610e3078ce93ba6ae9
Danilo Catapan <dcatapan@gmail.com>
2018-07-22 23:12:51 -0300

    Initial Commit

---
+++ Student.Entity/Context/StudentContext.cs
@@ -0,0 +1,29 @@
+﻿using Student.Entity.EntityConfig;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Entity
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(100));
+
+            modelBuilder.Configurations.Add(new ConfigurationStudent());
+        }
+    }
+}
---
+++ Student.Entity/EntityConfig/ConfigurationStudent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Entity.EntityConfig
+{
+    public class ConfigurationStudent : EntityTypeConfiguration<Student>
+    {
+        public ConfigurationStudent()
+        {
+            HasKey(s => s.Id);
+
+            Property(s => s.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(s => s.Name)
+                .IsVariableLength()
+                .IsRequired()
+                .HasMaxLength(150);
+
+            //mapStudent.Property(s => s.BirthDate)
+            //    .IsOptional();
+
+            //mapStudent.HasKey(s => s.Id);
+
+            Property(s => s.BirthDate).IsRequired();
+        }
+    }
+}
